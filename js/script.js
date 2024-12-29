a01dbcb68efa9c3c362befa70300d9c497c81415
oscarcheung <oscarcheung2023@gmail.com>
2024-12-29 15:32:41 -0500

    New About Section and Cleaning

--- js/script.js
+++ js/script.js
@@ -15,3 +15,15 @@ function type() {
 window.onload = () => {
     type();
 };
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+});
+
